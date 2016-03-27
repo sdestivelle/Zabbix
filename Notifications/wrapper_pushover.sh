3da9484cb10e95dc16c66305b984e22f268769ed
Gernot Leitgab <office@leitgab.at>
2016-03-27 18:46:11 +0200

    Use variables for easier handling of pushover notifications

--- Notifications/wrapper_pushover.sh
+++ Notifications/wrapper_pushover.sh
@@ -9,16 +9,22 @@
 # ---le--- -----par----- ---------------objet---------------
 # 2015/06/12 S. DESTIVELLE   Creation of the script
 # 2015/11/18 S. DESTIVELLE   Modification of the script (add device variable). Thanks to Marco
+# 2016/03/27 G. LEITGAB      Use variables for easier handling
 #
 #=====================================================================
 
-#cd /usr/local/share/zabbix/alertscripts
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # To do:
 # Add the Application Token
 # Add the name of your device declared at "https://pushover.net"
 # 
-App_Token = XXXX
-Device = XXX
+App_Token="XXXX"
+Device="XXX"
+Sound="classical"
+User="${1}"
+Title="${2}"
+Message="${3}"
 
 # The command
-./pushover.sh -d $Device -T $App_Token -s classical -U $1 -t $2 $3
+${DIR}/pushover.sh -d "${Device}" -T "${App_Token}" -s "${Sound}" -U "${User}" -t "${Title}" "${Message}"
